815a9aa3e9f6f8306d938bbdf3a4662a090136b2
Fareed619 <fareeddeeb722@gmail.com>
2025-01-31 19:26:55 +0200

    success edit

--- Backend/routes/paymentRoute.js
+++ Backend/routes/paymentRoute.js
@@ -27,8 +27,8 @@ router.post("/checkout", async (req, res) => {
         };
       }),
 
-      success_url: `${process.env.CLIENT_URL}/success/${_orderId}`,
-      cancel_url: `${process.env.CLIENT_URL}/cancel`,
+      success_url: `/success/${_orderId}`,
+      cancel_url: `/cancel`,
     });
     res.json({ url: session.url });
   } catch (e) {
