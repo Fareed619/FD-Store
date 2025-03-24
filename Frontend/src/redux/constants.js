90e287bc564b680e38b0a857d9b795cea0fd5c64
Fareed619 <fareeddeeb722@gmail.com>
2025-03-24 23:00:29 +0200

    edit api url

--- Frontend/src/redux/constants.js
+++ Frontend/src/redux/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:4000" : "https://fd-store-1.onrender.com";
+  import.meta.env.MODE === "development" ? "http://localhost:4000" : "https://fd-store-api.onrender.com";
 export const AUTH = "/api/auth";
 export const USERS = "/api/users";
 export const PROFILES = "/api/profiles";
