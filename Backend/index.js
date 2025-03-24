a7e1568e789ac5c617525960a1d504e56066faef
Fareed619 <fareeddeeb722@gmail.com>
2025-03-24 22:51:30 +0200

    last update

--- Backend/index.js
+++ Backend/index.js
@@ -28,13 +28,13 @@ app.use(All_Routes);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log("Server Listning on port " + PORT);
