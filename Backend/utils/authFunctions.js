d18cffaa8fed9b8a0fe45de61435de31101f9fa2
Fareed619 <fareeddeeb722@gmail.com>
2025-03-28 14:33:33 +0200

    edit

--- Backend/utils/authFunctions.js
+++ Backend/utils/authFunctions.js
@@ -15,8 +15,8 @@ export const generateToken = (userInfo, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
-    secure: process.env.NODE_ENV === "development",
-    sameSite: "strict",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "None",
     expires: 30 * 24 * 60 * 60 * 1000,
   });
 
